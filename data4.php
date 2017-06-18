3dd6ed7e19045571923b2d028531d71e887f4ee7
makerHo <524439109@qq.com>
2017-06-18 20:46:25 +0800

    FileData技术注册页面
    FileData技术注册页面

---
+++ data4.php
@@ -0,0 +1,8 @@
+<?php
+//sleep延缓代码执行；
+sleep(3);
+//$_POST:数组类型，可以获取前台以post方式传递的值
+//var_dump:打印变量信息
+var_dump($_POST);
+//$_FILES:获取上传文件的数据
+var_dump($_FILES);
--- js/register.js
+++ js/register.js
@@ -14,6 +14,9 @@ var pwd = document.querySelector(".pwd");
 var pwdFeedback = document.querySelector(".pwd_feedback");
 var repwd = document.querySelector(".repwd");
 var repwdFeedback = document.querySelector(".repwd_feedback");
+var regBtn = document.querySelector(".regBtn");
+var pic = document.querySelector(".pic");
+var feedback = document.querySelector(".feedback");
 
 var fd = new FormData();
 var xhr = new XMLHttpRequest();
@@ -37,10 +40,80 @@ var xhr = new XMLHttpRequest();
 	})
 	//密码验证
 	pwd.addEventListener("focus",function(){
-		
-	})
+		if(trim(pwd.value)==0){
+			pwdFeedback.innerHTML="Your password must be 8-16 characters, and include at least one lowercase letter, one uppercase letter, and a number!";
+	}else{
+		pwdFeedback.innerHTML="";
+	}})
 	pwd.addEventListener("keyup",function(){
-		if(){}
+		switch(valide_pwd(trim(pwd.value))){
+			case 3:
+				pwdFeedback.innerHTML="密码强度弱";
+				pwdFeedback.style.color="red";
+				break;
+			case 2:
+				pwdFeedback.innerHTML="密码强度中";
+				pwdFeedback.style.color="orange";
+				errorMsg="密码强度较中"
+				break;
+			case 1:
+				pwdFeedback.innerHTML="密码强度较强";
+				pwdFeedback.style.color="seagreen";
+				errorMsg="密码强度较弱"
+				break;
+			case 0:
+				pwdFeedback.innerHTML="密码强度高";
+				pwdFeedback.style.color="green";
+				errorMsg="密码强度高"
+				break;
+		}
 	});
+	//repwd 验正
+	repwd.addEventListener("focus",function(){
+		if(trim(repwd.value)==0){
+			repwdFeedback.innerHTML="Please re-enter your password!"
+		}else{
+			repwdFeedback.innerHTML="";
+		}
+	})
+	repwd.addEventListener("keyup",function(){
+		if(trim(pwd.value)==trim(repwd.value)){
+			repwdFeedback.innerHTML="The password is consistent";
+			repwdFeedback.style.color="green";
+		}else{
+			repwdFeedback.innerHTML="The password isn't consistent";
+			repwdFeedback.style.color="red";
+		}
+	})
+	//regBtn click 非空验证
+	regBtn.addEventListener("click",function(){
+		if(trim(email.value)==0){
+			emlFeedback.innerHTML="Please enter your e-mail address";
+			emlFeedback.style.color="red";
+			return false;
+		}else if(trim(pwd.value)==0){
+			pwdFeedback.innerHTML="Please enter your password address";
+			pwdFeedback.style.color="red";
+			return false;
+		}else if(trim(repwd.value)==0){
+			repwdFeedback.innerHTML="Please re-enter your password";
+			repwdFeedback.style.color="red";
+			return false;
+		}else{
+			xhr.open("post","data4.php");
+			feedback.innerHTML="loading..."
+			fd.append("email",email.value);
+			fd.append("pwd",pwd.value);
+			fd.append("pic",pic.files[0]);
+			xhr.send(fd);
+			xhr.addEventListener("readystatechange",function(){
+				if(xhr.readyState==4){
+					if(xhr.status==200){
+						feedback.innerHTML=xhr.responseText;
+					}
+				}
+			});
+		}
+	})
 //----------------------
 });
